78326e9d9edf3ea5bf037e246f2177f6b3935b20
Rohit Chanaliya <rohit.cse17@nituk.ac.in>
2020-08-16 10:48:32 +0530

    add button bar

--- lib/main.dart
+++ lib/main.dart
@@ -125,6 +125,27 @@ class _MyHomePageState extends State<MyHomePage> {
                       tooltip: 'Flight',
                     ),
                   ],
+                ),
+                Container(
+                  color: Colors.white70,
+                  child: ButtonBar(
+                    alignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.map),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.airport_shuttle),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.brush),
+                        splashColor: Colors.purple,
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
                 )
               ],
             ),
